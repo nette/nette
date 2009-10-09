d424fe7576af2493898715b8a95652c11d904a13
Matej Kravjar <matej.kravjar@gmail.com>
2009-10-10 07:16:21 +0800

    Nette\Tests: added shell script for Linux

---
+++ tests/run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# saves the path to this script's directory
+dir=` dirname $0 `
+
+# absolutizes the path if necessary
+if echo $dir | grep -v ^/ > /dev/null; then
+	dir=` pwd `/$dir
+fi
+
+# runs RunTests.php with script's arguments
+php "$dir/NetteTest/RunTests.php" $*
+
+# returns what script returned
+
