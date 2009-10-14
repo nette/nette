696be659b256cf4e4f0b25ac8ddde80d84b03e95
David Grudl <david@grudl.com>
2009-10-14 15:44:03 +0200

    Presenter: fixed terminate() back compatibility in AJAX

--- Nette/Application/Presenter.php
+++ Nette/Application/Presenter.php
@@ -241,7 +241,7 @@ abstract class Presenter extends Control implements IPresenter
 			$this->phase = self::PHASE_SHUTDOWN;
 
 			// back compatibility for use terminate() instead of sendPayload()
-			if ($this->isAjax() && !($this->response instanceof ForwardingResponse || $this->response instanceof JsonResponse) && (array) $this->payload) {
+			if ($this->isAjax() && !$this->response && (array) $this->payload) {
 				try { $this->sendPayload(); }
 				catch (AbortException $e) { }
 			}
