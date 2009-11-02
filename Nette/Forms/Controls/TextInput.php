95f103cca9fcc41e5fbb6b681b470be17e24915d
David Grudl <david@grudl.com>
2009-11-02 16:11:12 +0100

    Forms: TextInput with ~Form::LENGTH doesn't generate maxlength attr

--- Nette/Forms/Controls/TextInput.php
+++ Nette/Forms/Controls/TextInput.php
@@ -98,10 +98,10 @@ class TextInput extends TextBase
 
 	public function notifyRule(Rule $rule)
 	{
-		if (is_string($rule->operation) && strcasecmp($rule->operation, ':length') === 0) {
+		if (is_string($rule->operation) && strcasecmp($rule->operation, ':length') === 0 && !$rule->isNegative) {
 			$this->control->maxlength = is_array($rule->arg) ? $rule->arg[1] : $rule->arg;
 
-		} elseif (is_string($rule->operation) && strcasecmp($rule->operation, ':maxLength') === 0) {
+		} elseif (is_string($rule->operation) && strcasecmp($rule->operation, ':maxLength') === 0 && !$rule->isNegative) {
 			$this->control->maxlength = $rule->arg;
 		}
 
