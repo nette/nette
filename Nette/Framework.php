f56d93308b06583af4c0370486ae7195e4651a43
David Grudl <david@grudl.com>
2009-11-12 00:12:26 +0100

    opened 0.9.3-dev

--- Nette/Framework.php
+++ Nette/Framework.php
@@ -34,7 +34,7 @@ final class Framework
 	/**#@+ Nette Framework version identification */
 	const NAME = 'Nette Framework';
 
-	const VERSION = '0.9.2';
+	const VERSION = '0.9.3-dev';
 
 	const REVISION = '$WCREV$ released on $WCDATE$';
 
