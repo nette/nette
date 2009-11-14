7e61a16022a5fa3b0e42ccf792dc91b06fad6e7b
David Grudl <david@grudl.com>
2009-11-14 20:13:54 +0100

    Annotations: implemented PHP files parsing when reflection is not available

---
+++ tests/Nette/files/annotations.php
@@ -0,0 +1,74 @@
+<?php
+
+/*namespace Nette;*/
+
+/**
+ * @author john
+ */
+class AnnotatedClass1
+{
+	/** @var a */
+	public $a;
+
+	/** @var b */
+	public static $b;
+
+	/** @var c */
+	static public $c;
+
+	/** @var d */
+	protected $d;
+
+	/** @var e */
+	var $e;
+	var $f;
+
+	/** @var const */
+	const G = 0;
+	var $g;
+
+
+
+	/** @return a */
+	public function a() {}
+
+	/** @return b */
+	public static function b() {}
+
+	/** @return c */
+	static public function c() {}
+
+	/** @return d */
+	protected function d() {}
+
+	/** @return e */
+	function e() {}
+	function f() {}
+
+	/** @return g */
+	function & g() {}
+
+
+	/**#@+ @multiple */
+	var $m1;
+	var $m2;
+	/**#@-*/
+
+}
+
+
+/** @out of class */
+function a()
+{
+}
+
+
+/**
+ * @author jack
+ */
+class AnnotatedClass2 {
+}
+
+
+class AnnotatedClass3 {
+}
