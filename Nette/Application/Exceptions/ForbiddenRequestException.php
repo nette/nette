36af5e890e3e1641a70d7bb95248cd9c64e22b80
David Grudl <david@grudl.com>
2009-11-16 03:30:25 +0100

    added ForbiddenRequestException

---
+++ Nette/Application/Exceptions/ForbiddenRequestException.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Nette Framework
+ *
+ * Copyright (c) 2004, 2009 David Grudl (http://davidgrudl.com)
+ *
+ * This source file is subject to the "Nette license" that is bundled
+ * with this package in the file license.txt.
+ *
+ * For more information please see http://nettephp.com
+ *
+ * @copyright  Copyright (c) 2004, 2009 David Grudl
+ * @license    http://nettephp.com/license  Nette license
+ * @link       http://nettephp.com
+ * @category   Nette
+ * @package    Nette\Application
+ */
+
+/*namespace Nette\Application;*/
+
+
+
+require_once dirname(__FILE__) . '/../../Application/Exceptions/BadRequestException.php';
+
+
+
+/**
+ * Forbidden request exception - access denied.
+ *
+ * @author     David Grudl
+ * @copyright  Copyright (c) 2004, 2009 David Grudl
+ * @package    Nette\Application
+ */
+class ForbiddenRequestException extends BadRequestException
+{
+	/** @var int */
+	protected $defaultCode = 403;
+
+}
