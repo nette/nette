34822e77895f244e2ad3f7df0656aff7f0eed981
David Grudl <david@grudl.com>
2009-12-17 22:22:37 +0100

    Skeleton: added LoginPresenter & UsersModel

---
+++ tools/Skeleton/app/presenters/LoginPresenter.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * My Application
+ *
+ * @copyright  Copyright (c) 2009 John Doe
+ * @package    MyApplication
+ */
+
+/*use Nette\Environment;*/
+/*use Nette\Application\AppForm;*/
+/*use Nette\Forms\Form;*/
+/*use Nette\Security\AuthenticationException;*/
+
+
+/**
+ * Login / logout presenters.
+ *
+ * @author     John Doe
+ * @package    MyApplication
+ */
+class LoginPresenter extends BasePresenter
+{
+
+
+	/**
+	 * Login form component factory.
+	 * @return mixed
+	 */
+	protected function createComponentLoginForm()
+	{
+		$form = new AppForm;
+		$form->addText('username', 'Username:')
+			->addRule(Form::FILLED, 'Please provide a username.');
+
+		$form->addPassword('password', 'Password:')
+			->addRule(Form::FILLED, 'Please provide a password.');
+
+		$form->addCheckbox('remember', 'Remember me on this computer');
+
+		$form->addSubmit('login', 'Login');
+
+		$form->onSubmit[] = array($this, 'loginFormSubmitted');
+		return $form;
+	}
+
+
+
+	public function loginFormSubmitted($form)
+	{
+		try {
+			$values = $form->values;
+			if ($values['remember']) {
+				$this->getUser()->setExpiration('+ 14 days', FALSE);
+			} else {
+				$this->getUser()->setExpiration('+ 20 minutes', TRUE);
+			}
+			$this->getUser()->authenticate($values['username'], $values['password']);
+			$this->redirect('Homepage:');
+
+		} catch (AuthenticationException $e) {
+			$form->addError($e->getMessage());
+		}
+	}
+
+}
