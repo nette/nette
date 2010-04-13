ccc79e108ec15cb4871e8bd62ab6e6f5b39b2840
David Grudl <david@grudl.com>
2010-04-14 00:40:14 +0200

    Released version 0.9.4

--- Nette/Framework.php
+++ Nette/Framework.php
@@ -26,7 +26,7 @@ final class Framework
 	/**#@+ Nette Framework version identification */
 	const NAME = 'Nette Framework';
 
-	const VERSION = '0.9.4-dev';
+	const VERSION = '0.9.4';
 
 	const REVISION = '$WCREV$ released on $WCDATE$';
 
