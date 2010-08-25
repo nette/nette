7955d162c514c3ac2ea881d7387035acf005b793
David Grudl <david@grudl.com>
2010-08-26 01:05:20 +0200

    example CD-collection small refactoring (removed Environment)

--- examples/CD-collection/app/presenters/BasePresenter.php
+++ examples/CD-collection/app/presenters/BasePresenter.php
@@ -1,7 +1,6 @@
 <?php
 
-use Nette\Environment,
-	Nette\Application\Presenter;
+use Nette\Application\Presenter;
 
 
 abstract class BasePresenter extends Presenter
@@ -9,7 +8,8 @@ abstract class BasePresenter extends Presenter
 
 	protected function beforeRender()
 	{
-		$user = Environment::getUser();
+		$this->getSession()->start();
+		$user = $this->getUser();
 		$this->template->user = $user->isLoggedIn() ? $user->getIdentity() : NULL;
 	}
 
