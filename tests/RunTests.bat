620b235c970d6af9144ee311fda5c9176a0529d2
David Grudl <david@grudl.com>
2010-09-17 16:06:47 +0200

    NetteTest: moved config.ini support

--- tests/RunTests.bat
+++ tests/RunTests.bat
@@ -1,1 +1,1 @@
-@php.exe W:\Nette\_nette\tests\NetteTest\RunTests.php -p c:\php\php53\php-cgi.exe -c W:\Nette\_nette\tests\php.ini %1
+@php.exe %~dp0NetteTest\RunTests.php -p php-cgi.exe -c %~dp0php.ini %1
