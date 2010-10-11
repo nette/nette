4fbed5fe6366178b990e74a786dbd1fc76e8b3c6
Jakub Vrana <jakub@vrana.cz>
2010-10-11 12:02:22 +0200

    HttpUploadedFile: getContents()

--- Nette/Web/HttpUploadedFile.php
+++ Nette/Web/HttpUploadedFile.php
@@ -197,4 +197,16 @@ class HttpUploadedFile extends Nette\Object
 		return $this->isOk() ? @getimagesize($this->tmpName) : NULL; // @ - files smaller than 12 bytes causes read error
 	}
 
+
+
+	/**
+	 * Get file contents
+	 * @return string
+	 */
+	public function getContents()
+	{
+		// future implementation can try to work around safe_mode and open_basedir limitations
+		return $this->isOk() ? file_get_contents($this->tmpName) : NULL;
+	}
+
 }
