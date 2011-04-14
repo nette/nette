ea9fbfba0e6e233e69a612e3d7cf387e22e20d57
David Grudl <david@grudl.com>
2011-04-15 01:48:14 +0200

    shortcuts.php moved to loader.php

--- Nette/loader.php
+++ Nette/loader.php
@@ -25,7 +25,11 @@ if (PHP_VERSION_ID < 50200) {
 }
 */
 
+error_reporting(E_ALL | E_STRICT);
 @set_magic_quotes_runtime(FALSE); // @ - deprecated since PHP 5.3.0
+iconv_set_encoding('internal_encoding', 'UTF-8');
+extension_loaded('mbstring') && mb_internal_encoding('UTF-8');
+@header('X-Powered-By: Nette Framework'); // @ - headers may be sent
 
 
 
@@ -39,7 +43,6 @@ define('NETTE_PACKAGE', '5.3');
 
 
 
-require_once __DIR__ . '/Utils/shortcuts.php';
 require_once __DIR__ . '/Utils/exceptions.php';
 require_once __DIR__ . '/Utils/Framework.php';
 require_once __DIR__ . '/Utils/Object.php';
@@ -51,3 +54,27 @@ require_once __DIR__ . '/Loaders/NetteLoader.php';
 
 
 Nette\Loaders\NetteLoader::getInstance()->register();
+
+
+
+/**
+ * Nette\Callback factory.
+ * @param  mixed   class, object, function, callback
+ * @param  string  method
+ * @return Nette\Callback
+ */
+function callback($callback, $m = NULL)
+{
+	return ($m === NULL && $callback instanceof Nette\Callback) ? $callback : new Nette\Callback($callback, $m);
+}
+
+
+
+/**
+ * Nette\Debug::dump shortcut.
+ */
+function dump($var)
+{
+	foreach (func_get_args() as $arg) Nette\Debug::dump($arg);
+	return $var;
+}
