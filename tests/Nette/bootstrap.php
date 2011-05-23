d6c8c6ca7bd0e1fed6e4b57e68ae59fbed67cb7d
David Grudl <david@grudl.com>
2011-05-23 23:29:30 +0200

    tests: runs in strict mode, all warnings and notices are treated as fatal errors

--- tests/Nette/bootstrap.php
+++ tests/Nette/bootstrap.php
@@ -11,15 +11,30 @@ require __DIR__ . '/../Test/TestHelpers.php';
 require __DIR__ . '/../Test/Assert.php';
 require __DIR__ . '/../../Nette/loader.php';
 
+
 // configure environment
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', TRUE);
 ini_set('html_errors', FALSE);
 ini_set('log_errors', FALSE);
 
+
 // create temporary directory
 define('TEMP_DIR', __DIR__ . '/../tmp');
 @mkdir(TEMP_DIR);
+
+
+// catch unexpected errors/warnings/notices
+class TestErrorException extends Exception
+{}
+
+set_error_handler(function($severity, $message, $file, $line) {
+	if (($severity & error_reporting()) === $severity) {
+		throw new TestErrorException("$message in $file:$line");
+	}
+	return FALSE;
+});
+
 
 $_SERVER = array_intersect_key($_SERVER, array_flip(array('PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS')));
 $_SERVER['REQUEST_TIME'] = 1234567890;
@@ -28,6 +43,7 @@ $_ENV = $_GET = $_POST = array();
 if (PHP_SAPI !== 'cli') {
 	header('Content-Type: text/plain; charset=utf-8');
 }
+
 
 if (extension_loaded('xdebug')) {
 	xdebug_disable();
