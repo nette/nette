817d5f37fd505500310262a0aaac12e9ebf47f5a
bojovyletoun <Mat@.(none)>
2011-06-10 02:28:29 +0200

    bug Rules

--- Nette/Forms/Rules.php
+++ Nette/Forms/Rules.php
@@ -254,7 +254,6 @@ final class Rules extends Nette\Object implements \IteratorAggregate
 
 	public static function formatMessage($rule, $withValue)
 	{
-		$isHtml = $message instanceof Html;
 		$message = $rule->message;
 		$isHtml = $message instanceof Html;
 		if (!isset($message)) { // report missing message by notice
