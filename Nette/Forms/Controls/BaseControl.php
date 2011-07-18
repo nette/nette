5ab05913ed7460a3e4b531759e3b1e33d6b6e9c0
MartyIX <vseticka.martin@gmail.com>
2011-07-18 14:01:10 +0200

    Added phpdoc for setOption [without whitespace characters]
    Copied from https://github.com/nette/nette/blob/master/Nette/Forms/ControlGroup.php#L77

--- Nette/Forms/Controls/BaseControl.php
+++ Nette/Forms/Controls/BaseControl.php
@@ -188,10 +188,10 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 
 	/**
 	 * Sets user-specific option.
-	 * Options recognized by DefaultFormRenderer 
-	 * - 'description' - textual or Html object description 	 
+	 * Options recognized by DefaultFormRenderer
+	 * - 'description' - textual or Html object description
 	 * 
-	 * @param  string key	     
+	 * @param  string key
 	 * @param  mixed  value
 	 * @return BaseControl  provides a fluent interface
 	 */
