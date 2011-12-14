d7391b80a8e02fe8d721caa890dbdccd8648526f
David Grudl <david@grudl.com>
2011-12-14 05:04:45 +0100

    Mail: fixed Content-Disposition header encoding [Closes #325]

--- Nette/Mail/MimePart.php
+++ Nette/Mail/MimePart.php
@@ -153,6 +153,10 @@ class MimePart extends Nette\Object
 			}
 			return substr($s, 0, -1); // last comma
 
+		} elseif (preg_match('#^(\S+; (?:file)?name=)"(.*)"$#', $this->headers[$name], $m)) { // Content-Disposition
+			$offset += strlen($m[1]);
+			return $m[1] . '"' . self::encodeHeader($m[2], $offset) . '"';
+
 		} else {
 			return self::encodeHeader($this->headers[$name], $offset);
 		}
