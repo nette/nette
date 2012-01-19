83a8de17f5f53969df9c84a9e9a7b65b064fe310
David Grudl <david@grudl.com>
2012-01-19 06:25:31 +0100

    umask(0) called in loader.php [Closes #472]

--- Nette/loader.php
+++ Nette/loader.php
@@ -29,6 +29,7 @@ error_reporting(E_ALL | E_STRICT);
 @set_magic_quotes_runtime(FALSE); // @ - deprecated since PHP 5.3.0
 iconv_set_encoding('internal_encoding', 'UTF-8');
 extension_loaded('mbstring') && mb_internal_encoding('UTF-8');
+umask(0);
 @header('X-Powered-By: Nette Framework'); // @ - headers may be sent
 @header('Content-Type: text/html; charset=utf-8'); // @ - headers may be sent
 
