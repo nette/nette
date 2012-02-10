f88b62e29d2f0aed719377cec9cae1a9a60e184b
David Grudl <david@grudl.com>
2012-02-10 14:30:33 +0100

    DI: nested separator changed to double __ [Closes #523]

--- Nette/DI/Container.php
+++ Nette/DI/Container.php
@@ -361,7 +361,7 @@ class Container extends Nette\FreezableObject implements IContainer
 	public static function getMethodName($name, $isService = TRUE)
 	{
 		$uname = ucfirst($name);
-		return ($isService ? 'createService' : 'create') . ($name === $uname ? '_' : '') . strtr($uname, '.', '_');
+		return ($isService ? 'createService' : 'create') . ($name === $uname ? '__' : '') . str_replace('.', '__', $uname);
 	}
 
 }
