63fb3c61f900fddb3f73c2e02057caec582e3379
Filip Procházka <hosiplan@gmail.com>
2012-02-22 21:47:17 +0100

    Config: support for constants in arguments [Closes #550]

--- Nette/DI/ContainerBuilder.php
+++ Nette/DI/ContainerBuilder.php
@@ -608,6 +608,9 @@ class ContainerBuilder extends Nette\Object
 			} elseif ($service = $that->getServiceName($val, $self)) {
 				$val = $service === $self ? '$service' : $that->formatStatement(new Statement($val));
 				$val = new PhpLiteral($val);
+
+			} elseif (is_string($val) && preg_match('#^[\w\\\\]*::[A-Z][A-Z0-9_]*\z#', $val, $m)) {
+				$val = new PhpLiteral(ltrim($val, ':'));
 			}
 		});
 		return PhpHelpers::formatArgs($statement, $args);
