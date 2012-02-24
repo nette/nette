e6f489a1d3b940881a4865c6992193fb0d3c2de8
David Grudl <david@grudl.com>
2012-02-24 23:08:36 +0100

    Tests: database tests are serialized

--- tests/Nette/Database/connect.inc.php
+++ tests/Nette/Database/connect.inc.php
@@ -19,9 +19,10 @@ require __DIR__ . '/../bootstrap.php';
 
 try {
 	$connection = new Database\Connection('mysql:host=localhost', 'root');
-	Database\Helpers::loadFromFile($connection, __DIR__ . '/nette_test.sql');
-
 } catch (PDOException $e) {
 	TestHelpers::skip('Requires corretly configured mysql connection and "nette_test" database.');
-
 }
+
+flock($lock = fopen(TEMP_DIR . '/../lock', 'w'), LOCK_EX);
+
+Database\Helpers::loadFromFile($connection, __DIR__ . '/nette_test.sql');
