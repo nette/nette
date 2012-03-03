bdde205c88c4637e983535219f628a0f2efbaccc
Tomáš Windsor <windsor@seznam.cz>
2012-03-03 16:47:33 +0100

    DI\Diagnostics: fixed formatting of service and factory names

--- Nette/DI/Diagnostics/ContainerPanel.php
+++ Nette/DI/Diagnostics/ContainerPanel.php
@@ -62,7 +62,7 @@ class ContainerPanel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 		$factories = array();
 		foreach (Nette\Reflection\ClassType::from($this->container)->getMethods() as $method) {
 			if (preg_match('#^create(Service)?(.+)$#', $method->getName(), $m)) {
-				$name = strtr(strtolower(substr($m[2], 0, 1)) . substr($m[2], 1), '_', '.');
+				$name = str_replace('__', '.', strtolower(substr($m[2], 0, 1)) . substr($m[2], 1));
 				if ($m[1]) {
 					$services[$name] = $method->getAnnotation('return');
 				} elseif ($method->isPublic()) {
