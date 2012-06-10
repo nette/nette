185cfceedde7a815523ea18840f35ba1ef40ce79
David Grudl <david@grudl.com>
2012-06-10 22:20:18 +0200

    ObjectMixin: extension method support moved from Nette\Reflection to Nette\ObjectMixin (BC break)

--- Nette/Reflection/ClassType.php
+++ Nette/Reflection/ClassType.php
@@ -52,10 +52,6 @@ use Nette,
 class ClassType extends \ReflectionClass
 {
 
-	/** @var array (method => array(type => callable)) */
-	private static $extMethods;
-
-
 
 	/**
 	 * @param  string|object
@@ -71,72 +67,6 @@ class ClassType extends \ReflectionClass
 	public function __toString()
 	{
 		return $this->getName();
-	}
-
-
-
-	/**
-	 * Adds a method to class.
-	 * @param  string  method name
-	 * @param  mixed   callable
-	 * @return ClassType  provides a fluent interface
-	 */
-	public function setExtensionMethod($name, $callback)
-	{
-		$l = & self::$extMethods[strtolower($name)];
-		$l[strtolower($this->getName())] = callback($callback);
-		$l[''] = NULL;
-		return $this;
-	}
-
-
-
-	/**
-	 * Returns extension method.
-	 * @param  string  method name
-	 * @return mixed
-	 */
-	public function getExtensionMethod($name)
-	{
-		/*5.2* if (self::$extMethods === NULL || $name === NULL) { // for backwards compatibility
-			$list = get_defined_functions(); // names are lowercase!
-			foreach ($list['user'] as $fce) {
-				$pair = explode('_prototype_', $fce);
-				if (count($pair) === 2) {
-					self::$extMethods[$pair[1]][$pair[0]] = callback($fce);
-					self::$extMethods[$pair[1]][''] = NULL;
-				}
-			}
-			if ($name === NULL) {
-				return NULL;
-			}
-		}
-		*/
-
-		$class = strtolower($this->getName());
-		$l = & self::$extMethods[strtolower($name)];
-
-		if (empty($l)) {
-			return FALSE;
-
-		} elseif (isset($l[''][$class])) { // cached value
-			return $l[''][$class];
-		}
-
-		$cl = $class;
-		do {
-			if (isset($l[$cl])) {
-				return $l[''][$class] = $l[$cl];
-			}
-		} while (($cl = strtolower(get_parent_class($cl))) !== '');
-
-		foreach (class_implements($class) as $cl) {
-			$cl = strtolower($cl);
-			if (isset($l[$cl])) {
-				return $l[''][$class] = $l[$cl];
-			}
-		}
-		return $l[''][$class] = FALSE;
 	}
 
 
