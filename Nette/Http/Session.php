42b14f616f71a8ab7278c00925e0ba7c5f3feede
David Grudl <david@grudl.com>
2012-11-22 01:30:06 +0100

    Http\Session: session is not regenerated after 30 minutes; only new session is regenerated

--- Nette/Http/Session.php
+++ Nette/Http/Session.php
@@ -33,9 +33,6 @@ class Session extends Nette\Object
 	/** Default file lifetime is 3 hours */
 	const DEFAULT_FILE_LIFETIME = 10800;
 
-	/** Regenerate session ID every 30 minutes */
-	const REGENERATE_INTERVAL = 1800;
-
 	/** @var bool  has been session ID regenerated? */
 	private $regenerated;
 
@@ -116,12 +113,10 @@ class Session extends Nette\Object
 		$nf = & $_SESSION['__NF'];
 		@$nf['C']++;
 
-		// session regenerate every 30 minutes
-		$nfTime = & $nf['Time'];
-		$time = time();
-		if ($time - $nfTime > self::REGENERATE_INTERVAL) {
-			$this->regenerated = $this->regenerated || isset($nfTime);
-			$nfTime = $time;
+		// regenerate empty session
+		if (empty($nf['Time'])) {
+			$nf['Time'] = time();
+			$this->regenerated = TRUE;
 		}
 
 		// browser closing detection
