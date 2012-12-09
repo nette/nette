6eef4b372309980cd297fc28219fc234e4dd709e
hrach <hrach.cz@gmail.com>
2012-12-09 19:54:48 +0100

    Database: refactored SqlBuilder dependencies

--- Nette/Database/Table/GroupedSelection.php
+++ Nette/Database/Table/GroupedSelection.php
@@ -104,7 +104,7 @@ class GroupedSelection extends Selection
 
 	public function aggregation($function)
 	{
-		$aggregation = & $this->getRefTable($refPath)->aggregation[$refPath . $function . $this->sqlBuilder->buildSelectQuery() . json_encode($this->sqlBuilder->getParameters())];
+		$aggregation = & $this->getRefTable($refPath)->aggregation[$refPath . $function . $this->getSql() . json_encode($this->sqlBuilder->getParameters())];
 
 		if ($aggregation === NULL) {
 			$aggregation = array();
@@ -147,7 +147,7 @@ class GroupedSelection extends Selection
 			return;
 		}
 
-		$hash = md5($this->sqlBuilder->buildSelectQuery() . json_encode($this->sqlBuilder->getParameters()));
+		$hash = md5($this->getSql() . json_encode($this->sqlBuilder->getParameters()));
 
 		$referencing = & $this->getRefTable($refPath)->referencing[$refPath . $hash];
 		$this->rows = & $referencing['rows'];
