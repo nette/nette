0622617df91e648e7a0d948b21b33afb7ae08793
David Grudl <david@grudl.com>
2013-02-19 20:28:21 +0100

    Latte: recognizes {Namespace\Class::member}

--- Nette/Latte/Parser.php
+++ Nette/Latte/Parser.php
@@ -339,7 +339,7 @@ class Parser extends Nette\Object
 	{
 		$match = Strings::match($tag, '~^
 			(
-				(?P<name>\?|/?[a-z]\w*+(?:[.:]\w+)*+(?!::|\())|   ## ?, name, /name, but not function( or class::
+				(?P<name>\?|/?[a-z]\w*+(?:[.:]\w+)*+(?!::|\(|\\\\))|   ## ?, name, /name, but not function( or class:: or namespace\
 				(?P<noescape>!?)(?P<shortname>/?[=\~#%^&_]?)      ## !expression, !=expression, ...
 			)(?P<args>.*?)
 			(?P<modifiers>\|[a-z](?:'.Parser::RE_STRING.'|[^\'"])*)?
