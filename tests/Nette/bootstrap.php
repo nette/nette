285971e189013d693bd8363dbad1b8ac4c551b33
David Grudl <david@grudl.com>
2013-02-27 16:41:54 +0100

    tests: temp dir is created non-recursive

--- tests/Nette/bootstrap.php
+++ tests/Nette/bootstrap.php
@@ -22,6 +22,7 @@ date_default_timezone_set('Europe/Prague');
 
 // create temporary directory
 define('TEMP_DIR', __DIR__ . '/../tmp/' . getmypid());
+@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
 Tester\Helpers::purge(TEMP_DIR);
 
 
