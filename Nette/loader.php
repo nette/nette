c0332ac55877cc6e678a3cd0679caaa3fc48c55b
David Grudl <david@grudl.com>
2013-03-08 16:44:18 +0100

    Released version 2.0.10

--- Nette/loader.php
+++ Nette/loader.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Nette Framework (version 2.0.9 released on $WCDATE$, http://nette.org)
+ * Nette Framework (version 2.0.10 released on $WCDATE$, http://nette.org)
  *
  * Copyright (c) 2004, 2013 David Grudl (http://davidgrudl.com)
  *
@@ -40,7 +40,7 @@ umask(0);
  */
 define('NETTE', TRUE);
 define('NETTE_DIR', __DIR__);
-define('NETTE_VERSION_ID', 20009); // v2.0.9
+define('NETTE_VERSION_ID', 20010); // v2.0.10
 define('NETTE_PACKAGE', '5.3');
 
 
