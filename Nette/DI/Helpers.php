3e4e4177343cc542acff12544a952c62de5770ee
David Grudl <david@grudl.com>
2013-04-02 13:27:36 +0200

    DI: expand() will expand properties of stdClass objects too

--- Nette/DI/Helpers.php
+++ Nette/DI/Helpers.php
@@ -40,8 +40,12 @@ final class Helpers
 			}
 			return $res;
 
-		} elseif ($var instanceof Statement) {
-			return new Statement(self::expand($var->entity, $params, $recursive), self::expand($var->arguments, $params, $recursive));
+		} elseif ($var instanceof \stdClass || $var instanceof Statement) {
+			$res = clone $var;
+			foreach ($var as $key => $val) {
+				$res->$key = self::expand($val, $params, $recursive);
+			}
+			return $res;
 
 		} elseif (!is_string($var)) {
 			return $var;
