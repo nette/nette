e35b06824c32475901a1e41e627d9b699e27fb49
hrach <hrach.cz@gmail.com>
2013-04-03 13:07:08 +0200

    Database: added parseJoins test

---
+++ tests/Nette/Database/SqlBuilder.parseJoins().php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Test: Nette\Database\Table\SqlBuilder: parseJoins().
+ *
+ * @author     Jan Skrasek
+ * @package    Nette\Database
+ * @dataProvider? databases.ini
+ */
+
+require __DIR__ . '/connect.inc.php'; // create $connection
+
+Nette\Database\Helpers::loadFromFile($connection, __DIR__ . "/{$driverName}-nette_test2.sql");
+
+use Tester\Assert;
+use Nette\Database\Reflection\DiscoveredReflection;
+use Nette\Database\Table\SqlBuilder;
+
+
+
+class SqlBuilderMock extends SqlBuilder
+{
+	public function parseJoins(& $joins, & $query, $inner = FALSE)
+	{
+		parent::parseJoins($joins, $query);
+	}
+	public function buildQueryJoins(array $joins)
+	{
+		return parent::buildQueryJoins($joins);
+	}
+}
+
+$reflection = new DiscoveredReflection($connection);
+$sqlBuilder = new SqlBuilderMock('nUsers', $connection, $reflection);
+
+
+
+$joins = array();
+$query = 'WHERE :nusers_ntopics.topic.priorit.id IS NULL';
+$sqlBuilder->parseJoins($joins, $query);
+$join = $sqlBuilder->buildQueryJoins($joins);
+Assert::equal('WHERE priorit.id IS NULL', $query);
+Assert::equal(
+	'LEFT JOIN nusers_ntopics ON nUsers.nUserId = nusers_ntopics.nUserId ' .
+	'LEFT JOIN ntopics AS topic ON nusers_ntopics.nTopicId = topic.nTopicId ' .
+	'LEFT JOIN npriorities AS priorit ON topic.nPriorityId = priorit.nPriorityId',
+	trim($join)
+);
