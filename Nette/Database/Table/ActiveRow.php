e0d809d573d6163ba4516330424fa61e7379853e
hrach <hrach.cz@gmail.com>
2013-04-18 22:05:49 +0200

    Database: removed deprecated __set & __unset from ActiveRow, is is now read-only

--- Nette/Database/Table/ActiveRow.php
+++ Nette/Database/Table/ActiveRow.php
@@ -34,9 +34,6 @@ class ActiveRow implements \IteratorAggregate, IRow
 	/** @var bool */
 	private $dataRefreshed = FALSE;
 
-	/** @var array of new values {@see ActiveRow::update()} */
-	private $modified = array();
-
 	/** @var bool */
 	private $isModified = FALSE;
 
@@ -182,15 +179,6 @@ class ActiveRow implements \IteratorAggregate, IRow
 	 */
 	public function update($data)
 	{
-		if ($data instanceof \Traversable) {
-			$data = iterator_to_array($data);
-		}
-		if ($data === NULL) {
-			$data = $this->modified;
-		}
-		$this->data = $data + $this->data;
-		$this->modified = $data + $this->modified;
-
 		$selection = $this->table->getConnection()
 			->table($this->table->getName())
 			->wherePrimary($this->getPrimary());
@@ -202,7 +190,6 @@ class ActiveRow implements \IteratorAggregate, IRow
 				throw new Nette\InvalidStateException('Database refetch failed; row does not exist!');
 			}
 			$this->data = $row->data;
-			$this->modified = array();
 			return TRUE;
 		} else {
 			return FALSE;
@@ -298,9 +285,7 @@ class ActiveRow implements \IteratorAggregate, IRow
 
 	public function __set($key, $value)
 	{
-		trigger_error('Changing properties of ActiveRow is deprecated; use update() instead.', E_USER_DEPRECATED);
-		$this->data[$key] = $value;
-		$this->modified[$key] = $value;
+		throw new Nette\DeprecatedException('ActiveRow is read-only; use update() method instead.');
 	}
 
 
@@ -341,19 +326,13 @@ class ActiveRow implements \IteratorAggregate, IRow
 
 	public function __unset($key)
 	{
-		trigger_error('Unsetting properties of ActiveRow is deprecated.', E_USER_DEPRECATED);
-		unset($this->data[$key]);
-		unset($this->modified[$key]);
+		throw new Nette\DeprecatedException('ActiveRow is read-only.');
 	}
 
 
 
 	protected function accessColumn($key, $selectColumn = TRUE)
 	{
-		if (isset($this->modified[$key])) {
-			return;
-		}
-
 		$this->table->accessColumn($key, $selectColumn);
 		if ($this->table->getDataRefreshed() && !$this->dataRefreshed) {
 			$this->data = $this->table[$this->getSignature()]->data;
