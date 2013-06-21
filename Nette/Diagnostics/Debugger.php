b4d601af50acd4a04b9d1a4849f4b5596ca349c4
David Grudl <david@grudl.com>
2013-06-21 19:59:37 +0200

    Debugger: fixed missing $mailer after reference is created

--- Nette/Diagnostics/Debugger.php
+++ Nette/Diagnostics/Debugger.php
@@ -117,10 +117,10 @@ final class Debugger
 	public static $email;
 
 	/** @deprecated */
-	public static $mailer;
+	public static $mailer = array('Nette\Diagnostics\Logger', 'defaultMailer');
 
 	/** @deprecated */
-	public static $emailSnooze;
+	public static $emailSnooze = 172800;
 
 	/** {@link Debugger::log()} and {@link Debugger::fireLog()} */
 	const DEBUG = 'debug',
