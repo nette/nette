34f0564d698561004aff6fc5ce910ba4ff1696ed
David Grudl <david@grudl.com>
2013-08-05 21:04:53 +0200

    Nette\Framework: added constant VERSION_ID

--- Nette/common/Framework.php
+++ Nette/common/Framework.php
@@ -25,6 +25,7 @@ final class Framework
 	/** Nette Framework version identification */
 	const NAME = 'Nette Framework',
 		VERSION = '2.0.11',
+		VERSION_ID = 20011,
 		REVISION = '$WCREV$ released on $WCDATE$';
 
 	/** @var bool set to TRUE if your host has disabled function ini_set */
