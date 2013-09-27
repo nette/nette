nette/nette

commit bec4fea149b28e07b8abc4fe64d561b41adc7d8d

Forms: submitted data validation is provided by Form::getHttpData(), called from BaseControl::loadHttpDate() (a little revolution)

Nette/Forms/Controls/ImageButton.php
@@ -36,17 +36,15 @@ class ImageButton extends SubmitButton
 
 
 	/**
-	 * Sets coordinates as a value if available.
-	 * @param  bool|array
-	 * @return ImageButton  provides a fluent interface
+	 * Loads HTTP data.
+	 * @return void
 	 */
-	public function setValue($value)
+	public function loadHttpData()
 	{
-		parent::setValue($value);
-		if (is_array($value) && isset($value[0], $value[1])) {
-			$this->value = array((int) $value[0], (int) $value[1]);
-		}
-		return $this;
+		parent::loadHttpData();
+		$this->value = $this->value
+			? array((int) array_shift($this->value), (int) array_shift($this->value))
+			: FALSE;
 	}
 
 

tests/Nette/Forms/Controls.ImageButton.loadData.phpt
@@ -54,9 +54,9 @@ test(function() { // malformed data
 	$form = new Form;
 	$input = $form->addImage('malformed1');
 	Assert::true( $input->isFilled() );
-	Assert::true( $input->getValue() );
+	Assert::same( array(1, 0), $input->getValue() );
 
 	$input = $form->addImage('malformed2');
-	Assert::true( $input->isFilled() );
-	Assert::same( array(1, 0), $input->getValue() );
+	Assert::false( $input->isFilled() );
+	Assert::false( $input->getValue() );
 });

Nette/Forms/Controls/BaseControl.php
@@ -70,9 +70,6 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 	/** @var string */
 	private $htmlId;
 
-	/** @var string */
-	private $htmlName;
-
 	/** @var Nette\Forms\Rules */
 	private $rules;
 
@@ -105,7 +102,6 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 	protected function attached($form)
 	{
 		if (!$this->disabled && $form instanceof Form && $form->isAnchored() && $form->isSubmitted()) {
-			$this->htmlName = NULL;
 			$this->loadHttpData();
 		}
 	}
@@ -123,22 +119,12 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 
 
 	/**
-	 * Returns HTML name of control.
-	 * @return string
+	 * Loads HTTP data.
+	 * @return void
 	 */
-	public function getHtmlName()
+	public function loadHttpData()
 	{
-		if ($this->htmlName === NULL) {
-			$name = str_replace(self::NAME_SEPARATOR, '][', $this->lookupPath('Nette\Forms\Form'), $count);
-			if ($count) {
-				$name = substr_replace($name, '', strpos($name, ']'), 1) . ']';
-			}
-			if (is_numeric($name) || in_array($name, array('attributes','children','elements','focus','length','reset','style','submit','onsubmit'))) {
-				$name = '_' . $name;
-			}
-			$this->htmlName = $name;
-		}
-		return $this->htmlName;
+		$this->setValue($this->getHttpData());
 	}
 
 
@@ -146,13 +132,19 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 	 * Loads HTTP data.
 	 * @return void
 	 */
-	public function loadHttpData()
+	public function getHttpData($type = Nette\Forms\Form::DATA_TEXT, $htmlTail = NULL)
 	{
-		$this->setRawValue(Nette\Utils\Arrays::get(
-			$this->getForm()->getHttpData(),
-			explode('[', strtr(str_replace(array('[]', ']'), '', $this->getHtmlName()), '.', '_')),
-			NULL
-		));
+		return $this->getForm()->getHttpData($this->getHtmlName() . $htmlTail, $type);
+	}
+
+
+	/**
+	 * Returns HTML name of control.
+	 * @return string
+	 */
+	public function getHtmlName()
+	{
+		return Nette\Forms\Helpers::generateHtmlName($this->lookupPath('Nette\Forms\Form'));
 	}
 
 
@@ -202,16 +194,6 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 			$this->setValue($value);
 		}
 		return $this;
-	}
-
-
-	/**
-	 * Sets control's submitted data.
-	 * @return BaseControl  provides a fluent interface
-	 */
-	protected function setRawValue($value)
-	{
-		return $this->setValue($value);
 	}
 
 

Nette/Forms/Controls/Checkbox.php
@@ -48,12 +48,6 @@ class Checkbox extends BaseControl
 	}
 
 
-	protected function setRawValue($value)
-	{
-		return $this->setValue(is_scalar($value) ? (bool) $value : FALSE);
-	}
-
-
 	/**
 	 * Is control filled?
 	 * @return bool

Nette/Forms/Controls/HiddenField.php
@@ -47,12 +47,6 @@ class HiddenField extends BaseControl
 	}
 
 
-	protected function setRawValue($value)
-	{
-		return $this->setValue(is_scalar($value) ? (string) $value : '');
-	}
-
-
 	/**
 	 * Generates control's HTML element.
 	 * @return Nette\Utils\Html

Nette/Forms/Controls/MultiSelectBox.php
@@ -25,6 +25,16 @@ class MultiSelectBox extends SelectBox
 
 
 	/**
+	 * Loads HTTP data.
+	 * @return void
+	 */
+	public function loadHttpData()
+	{
+		$this->value = array_keys(array_flip($this->getHttpData()));
+	}
+
+
+	/**
 	 * Sets selected items (by keys).
 	 * @param  array
 	 * @return MultiSelectBox  provides a fluent interface
@@ -39,7 +49,8 @@ class MultiSelectBox extends SelectBox
 		if ($diff = array_diff($values, array_keys($this->allowed))) {
 			throw new Nette\InvalidArgumentException("Values '" . implode("', '", $diff) . "' are out of range of current items.");
 		}
-		return $this->setRawValue($values);
+		$this->value = array_keys(array_flip($values));
+		return $this;
 	}
 
 
@@ -50,19 +61,6 @@ class MultiSelectBox extends SelectBox
 	public function getValue()
 	{
 		return array_values(array_intersect($this->value, array_keys($this->allowed)));
-	}
-
-
-	protected function setRawValue($values)
-	{
-		$res = array();
-		foreach (is_array($values) ? $values : array($values) as $value) {
-			if (is_scalar($value)) {
-				$res[$value] = NULL;
-			}
-		}
-		$this->value = array_keys($res);
-		return $this;
 	}
 
 

Nette/Forms/Controls/RadioList.php
@@ -53,6 +53,19 @@ class RadioList extends BaseControl
 
 
 	/**
+	 * Loads HTTP data.
+	 * @return void
+	 */
+	public function loadHttpData()
+	{
+		$this->value = $this->getHttpData();
+		if ($this->value !== NULL) {
+			$this->value = key(array($this->value => NULL));
+		}
+	}
+
+
+	/**
 	 * Sets selected radio value.
 	 * @param  string
 	 * @return RadioList  provides a fluent interface
@@ -62,7 +75,8 @@ class RadioList extends BaseControl
 		if (!isset($this->items[$value]) && $value !== NULL) {
 			throw new Nette\InvalidArgumentException("Value '$value' is out of range of current items.");
 		}
-		return $this->setRawValue($value);
+		$this->value = $value === NULL ? NULL : key(array($value => NULL));
+		return $this;
 	}
 
 
@@ -76,18 +90,6 @@ class RadioList extends BaseControl
 			trigger_error(__METHOD__ . '(TRUE) is deprecated; use getRawValue() instead.', E_USER_DEPRECATED);
 		}
 		return ($raw || isset($this->items[$this->value])) ? $this->value : NULL;
-	}
-
-
-	protected function setRawValue($value)
-	{
-		if (is_scalar($value)) {
-			$foo = array($value => NULL);
-			$this->value = key($foo);
-		} else {
-			$this->value = NULL;
-		}
-		return $this;
 	}
 
 

Nette/Forms/Controls/SelectBox.php
@@ -56,6 +56,19 @@ class SelectBox extends BaseControl
 
 
 	/**
+	 * Loads HTTP data.
+	 * @return void
+	 */
+	public function loadHttpData()
+	{
+		$this->value = $this->getHttpData();
+		if ($this->value !== NULL) {
+			$this->value = key(array($this->value => NULL));
+		}
+	}
+
+
+	/**
 	 * Sets selected items (by keys).
 	 * @param  string
 	 * @return SelectBox  provides a fluent interface
@@ -65,7 +78,8 @@ class SelectBox extends BaseControl
 		if (!isset($this->allowed[$value]) && $value !== NULL) {
 			throw new Nette\InvalidArgumentException("Value '$value' is out of range of current items.");
 		}
-		return $this->setRawValue($value);
+		$this->value = $value === NULL ? NULL : key(array($value => NULL));
+		return $this;
 	}
 
 
@@ -76,18 +90,6 @@ class SelectBox extends BaseControl
 	public function getValue()
 	{
 		return isset($this->allowed[$this->value]) ? $this->value : NULL;
-	}
-
-
-	protected function setRawValue($value)
-	{
-		if (is_scalar($value)) {
-			$foo = array($value => NULL);
-			$this->value = key($foo);
-		} else {
-			$this->value = NULL;
-		}
-		return $this;
 	}
 
 

commit 21866717d7166293fc9ab2314be734310649c429

Forms: fixed ImageButton submittedBy detection
Bug & BC break was introduced in bec4fea149b28e07b8abc4fe64d561b41adc7d8d

Nette/Forms/Controls/ImageButton.php
@@ -36,15 +36,13 @@ class ImageButton extends SubmitButton
 
 
 	/**
-	 * Loads HTTP data.
-	 * @return void
+	 * Sets control's value.
+	 * @return self
 	 */
-	public function loadHttpData()
+	public function setValue($value)
 	{
-		parent::loadHttpData();
-		$this->value = $this->value
-			? array((int) array_shift($this->value), (int) array_shift($this->value))
-			: FALSE;
+		$this->value = $value ? array((int) array_shift($value), (int) array_shift($value)) : NULL;
+		return $this;
 	}
 
 

tests/Nette/Forms/Controls.ImageButton.loadData.phpt
@@ -41,7 +41,7 @@ test(function() { // missing data
 	$form = new Form;
 	$input = $form->addImage('missing');
 	Assert::false( $input->isFilled() );
-	Assert::false( $input->getValue() );
+	Assert::null( $input->getValue() );
 });
 
 
@@ -58,5 +58,5 @@ test(function() { // malformed data
 
 	$input = $form->addImage('malformed2');
 	Assert::false( $input->isFilled() );
-	Assert::false( $input->getValue() );
+	Assert::null( $input->getValue() );
 });

tests/Nette/Forms/Forms.submittedBy.phpt
@@ -1,0 +1,48 @@
+<?php
+
+/**
+ * Test: Nette\Forms HTTP data.
+ *
+ * @author     Jan Skrasek
+ * @package    Nette\Forms
+ */
+
+use Nette\Forms\Form;
+use Tester\Assert;
+
+
+require __DIR__ . '/../bootstrap.php';
+
+
+before(function() {
+	$_SERVER['REQUEST_METHOD'] = 'POST';
+	$_GET = $_POST = $_FILES = array();
+});
+
+
+test(function() {
+	$name = 'name';
+	$_POST = array(Form::TRACKER_ID => $name, 'send2' => 'whatever');
+
+	$form = new Form($name);
+	$btn1 = $form->addSubmit('send1');
+	$btn2 = $form->addSubmit('send2');
+	$btn3 = $form->addSubmit('send3');
+
+	Assert::true( $form->isSuccess() );
+	Assert::same( $btn2, $form->isSubmitted() );
+});
+
+
+test(function() {
+	$name = 'name';
+	$_POST = array(Form::TRACKER_ID => $name, 'send2' => array('x' => 1, 'y' => 1));
+
+	$form = new Form($name);
+	$btn1 = $form->addImage('send1');
+	$btn2 = $form->addImage('send2');
+	$btn3 = $form->addImage('send3');
+
+	Assert::true( $form->isSuccess() );
+	Assert::same( $btn2, $form->isSubmitted() );
+});