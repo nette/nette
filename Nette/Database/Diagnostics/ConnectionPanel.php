356f1d216f7821401166aa1f19899e5272005d25
David Grudl <david@grudl.com>
2013-12-13 03:07:18 +0100

    Database: added limit for logged queries into Debugger Bar

--- Nette/Database/Diagnostics/ConnectionPanel.php
+++ Nette/Database/Diagnostics/ConnectionPanel.php
@@ -26,8 +26,14 @@ class ConnectionPanel extends Nette\Object implements Nette\Diagnostics\IBarPane
 	/** @var int maximum SQL length */
 	static public $maxLength = 1000;
 
+	/** @var int */
+	public $maxQueries = 100;
+
 	/** @var int logged time */
 	private $totalTime = 0;
+
+	/** @var int */
+	private $count = 0;
 
 	/** @var array */
 	private $queries = array();
@@ -47,6 +53,7 @@ class ConnectionPanel extends Nette\Object implements Nette\Diagnostics\IBarPane
 		if ($this->disabled) {
 			return;
 		}
+
 		$source = NULL;
 		foreach (debug_backtrace(FALSE) as $row) {
 			if (isset($row['file']) && is_file($row['file']) && strpos($row['file'], NETTE_DIR . DIRECTORY_SEPARATOR) !== 0) {
@@ -56,8 +63,12 @@ class ConnectionPanel extends Nette\Object implements Nette\Diagnostics\IBarPane
 				break;
 			}
 		}
+
+		$this->count++;
 		$this->totalTime += $result->getTime();
-		$this->queries[] = array($result->queryString, $params, $result->getTime(), $result->rowCount(), $result->getConnection(), $source);
+		if ($this->count < $this->maxQueries) {
+			$this->queries[] = array($result->queryString, $params, $result->getTime(), $result->rowCount(), $result->getConnection(), $source);
+		}
 	}
 
 
@@ -83,7 +94,7 @@ class ConnectionPanel extends Nette\Object implements Nette\Diagnostics\IBarPane
 	{
 		return '<span title="Nette\\Database ' . htmlSpecialChars($this->name) . '">'
 			. '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAEYSURBVBgZBcHPio5hGAfg6/2+R980k6wmJgsJ5U/ZOAqbSc2GnXOwUg7BESgLUeIQ1GSjLFnMwsKGGg1qxJRmPM97/1zXFAAAAEADdlfZzr26miup2svnelq7d2aYgt3rebl585wN6+K3I1/9fJe7O/uIePP2SypJkiRJ0vMhr55FLCA3zgIAOK9uQ4MS361ZOSX+OrTvkgINSjS/HIvhjxNNFGgQsbSmabohKDNoUGLohsls6BaiQIMSs2FYmnXdUsygQYmumy3Nhi6igwalDEOJEjPKP7CA2aFNK8Bkyy3fdNCg7r9/fW3jgpVJbDmy5+PB2IYp4MXFelQ7izPrhkPHB+P5/PjhD5gCgCenx+VR/dODEwD+A3T7nqbxwf1HAAAAAElFTkSuQmCC" />'
-			. count($this->queries) . ' ' . (count($this->queries) === 1 ? 'query' : 'queries')
+			. $this->count . ' ' . ($this->count === 1 ? 'query' : 'queries')
 			. ($this->totalTime ? ' / ' . sprintf('%0.1f', $this->totalTime * 1000) . 'ms' : '')
 			. '</span>';
 	}
@@ -139,16 +150,17 @@ class ConnectionPanel extends Nette\Object implements Nette\Diagnostics\IBarPane
 			$s .= '</td><td>' . $rows . '</td></tr>';
 		}
 
-		return empty($this->queries) ? '' :
+		return $this->count ?
 			'<style class="nette-debug"> #nette-debug td.nette-DbConnectionPanel-sql { background: white !important }
 			#nette-debug .nette-DbConnectionPanel-source { color: #BBB !important } </style>
-			<h1 title="' . htmlSpecialChars($connection->getDsn()) . '">Queries: ' . count($this->queries)
+			<h1 title="' . htmlSpecialChars($connection->getDsn()) . '">Queries: ' . $this->count
 			. ($this->totalTime ? ', time: ' . sprintf('%0.3f', $this->totalTime * 1000) . ' ms' : '') . ', ' . htmlSpecialChars($this->name) . '</h1>
 			<div class="nette-inner nette-DbConnectionPanel">
 			<table>
 				<tr><th>Time&nbsp;ms</th><th>SQL Statement</th><th>Params</th><th>Rows</th></tr>' . $s . '
-			</table>
-			</div>';
+			</table>'
+			. (count($this->queries) < $this->count ? '<p>...and more</p>' : '')
+			. '</div>' : '';
 	}
 
 }
