43f59e2a5ac438e3f9f061d6f7f6ebe58c21d48f
Vlasta Neubauer <paranoiq@centrum.cz>
2013-12-24 01:13:54 +0100

    RouteList: do not change name of MicroPresenter
    this allows using micro presenter in module routers. otherwise it will prepend Nette:Micro with the module name

--- Nette/Application/Routers/RouteList.php
+++ Nette/Application/Routers/RouteList.php
@@ -40,7 +40,10 @@ class RouteList extends Nette\ArrayList implements Nette\Application\IRouter
 		foreach ($this as $route) {
 			$appRequest = $route->match($httpRequest);
 			if ($appRequest !== NULL) {
-				$appRequest->setPresenterName($this->module . $appRequest->getPresenterName());
+				$name = $appRequest->getPresenterName();
+				if (strncmp($name, 'Nette:', 6)) {
+					$appRequest->setPresenterName($this->module . $name);
+				}
 				return $appRequest;
 			}
 		}
