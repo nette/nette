8505b621c1198d1c651cd7f7cd7ec97ef00238b5
David Grudl <david@grudl.com>
2014-03-20 03:15:33 +0100

    __toString: when is called as method, exceptions are thrown

--- Nette/Forms/Form.php
+++ Nette/Forms/Form.php
@@ -585,7 +585,7 @@ class Form extends Container
 
 	/**
 	 * Renders form to string.
-	 * @return bool  can throw exceptions? (hidden parameter)
+	 * @return can throw exceptions? (hidden parameter)
 	 * @return string
 	 */
 	public function __toString()
@@ -594,11 +594,10 @@ class Form extends Container
 			return $this->getRenderer()->render($this);
 
 		} catch (\Exception $e) {
-			if (func_get_args() && func_get_arg(0)) {
+			if (func_num_args()) {
 				throw $e;
-			} else {
-				trigger_error("Exception in " . __METHOD__ . "(): {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", E_USER_ERROR);
 			}
+			trigger_error("Exception in " . __METHOD__ . "(): {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", E_USER_ERROR);
 		}
 	}
 
