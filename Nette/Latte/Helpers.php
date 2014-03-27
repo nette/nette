0cf93e9415eb6b708f92eefd74f3574e3bc05282
David Grudl <david@grudl.com>
2014-03-27 16:40:21 +0100

    Latte: optimizePhp() supports <?xml declarations

--- Nette/Latte/Helpers.php
+++ Nette/Latte/Helpers.php
@@ -28,18 +28,18 @@ class Helpers
 		$res = $php = '';
 		$lastChar = ';';
 		$tokens = new \ArrayIterator(token_get_all($source));
-		foreach ($tokens as $key => $token) {
+		foreach ($tokens as $n => $token) {
 			if (is_array($token)) {
 				if ($token[0] === T_INLINE_HTML) {
 					$lastChar = '';
 					$res .= $token[1];
 
 				} elseif ($token[0] === T_CLOSE_TAG) {
-					$next = isset($tokens[$key + 1]) ? $tokens[$key + 1] : NULL;
+					$next = isset($tokens[$n + 1]) ? $tokens[$n + 1] : NULL;
 					if (substr($res, -1) !== '<' && preg_match('#^<\?php\s*\z#', $php)) {
 						$php = ''; // removes empty (?php ?), but retains ((?php ?)?php
 
-					} elseif (is_array($next) && $next[0] === T_OPEN_TAG) { // remove ?)(?php
+					} elseif (is_array($next) && $next[0] === T_OPEN_TAG && (!isset($tokens[$n + 2][1]) || $tokens[$n + 2][1] !== 'xml')) { // remove ?)(?php
 						if (!strspn($lastChar, ';{}:/')) {
 							$php .= $lastChar = ';';
 						}
@@ -64,11 +64,22 @@ class Helpers
 					}
 
 				} elseif ($token[0] === T_ELSE || $token[0] === T_ELSEIF) {
-					if ($tokens[$key + 1] === ':' && $lastChar === '}') {
+					if ($tokens[$n + 1] === ':' && $lastChar === '}') {
 						$php .= ';'; // semicolon needed in if(): ... if() ... else:
 					}
 					$lastChar = '';
 					$php .= $token[1];
+
+				} elseif ($token[0] === T_OPEN_TAG && $token[1] === '<?' && isset($tokens[$n+1][1]) && $tokens[$n+1][1] === 'xml') {
+					$lastChar = '';
+					$php .= '<<?php ?>?';
+					for ($tokens->next(); $tokens->valid(); $tokens->next()) {
+						$token = $tokens->current();
+						$php .= is_array($token) ? $token[1] : $token;
+						if ($token[0] === T_CLOSE_TAG) {
+							break;
+						}
+					}
 
 				} else {
 					if (!in_array($token[0], array(T_WHITESPACE, T_COMMENT, T_DOC_COMMENT, T_OPEN_TAG))) {
--- tests/Nette/Latte/Helpers.optimizePhp().phpt
+++ tests/Nette/Latte/Helpers.optimizePhp().phpt
@@ -16,3 +16,7 @@ require __DIR__ . '/../bootstrap.php';
 $input = file_get_contents(__DIR__ . '/templates/optimize.phtml');
 $expected = file_get_contents(__DIR__ . '/expected/Helpers.optimizePhp().phtml');
 Assert::match($expected, Helpers::optimizePhp($input));
+
+Assert::match('<<?php ?>?xml version="1.0" ?>', Helpers::optimizePhp('<?xml version="1.0" ?>'));
+Assert::match('<<?php ?>?xml version="1.0" ?>', Helpers::optimizePhp('<?php ?><?xml version="1.0" ?>'));
+Assert::match('<?php echo "<?xml" ;', Helpers::optimizePhp('<?php echo "<?xml" ?>'));
