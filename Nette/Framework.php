fd75a4460b0e375359523f30824d701ccdeff90a
David Grudl <david@grudl.com>
2014-04-28 14:50:30 +0200

    Released version 2.2-RC2

--- Nette/Framework.php
+++ Nette/Framework.php
@@ -20,9 +20,9 @@ class Framework
 
 	/** Nette Framework version identification */
 	const NAME = 'Nette Framework',
-		VERSION = '2.2-rc1',
+		VERSION = '2.2-rc2',
 		VERSION_ID = 20200,
-		REVISION = 'released on 2014-04-20';
+		REVISION = 'released on 2014-04-28';
 
 
 	/**
