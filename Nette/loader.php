023889efbb8a366b223b1d3ac535929d98c797a8
David Grudl <david@grudl.com>
2014-05-05 10:31:37 +0200

    composer.json creates Nette Loader, aliases.php placed into loader.php

--- Nette/loader.php
+++ Nette/loader.php
@@ -17,6 +17,23 @@ require_once __DIR__ . '/Loaders/NetteLoader.php';
 
 Nette\Loaders\NetteLoader::getInstance()->register();
 
+array_walk(new ArrayIterator(array(
+	'Nette\Config\Configurator' => 'Nette\Configurator',
+	'Nette\Config\CompilerExtension' => 'Nette\DI\CompilerExtension',
+	'Nette\Diagnostics\Bar' => 'Tracy\Bar',
+	'Nette\Diagnostics\BlueScreen' => 'Tracy\BlueScreen',
+	'Nette\Diagnostics\Dumper' => 'Tracy\Dumper',
+	'Nette\Latte\CompileException' => 'Latte\CompileException',
+	'Nette\Latte\IMacro' => 'Latte\IMacro',
+	'Nette\Latte\Macros\MacroSet' => 'Latte\Macros\MacroSet',
+	'Nette\ArrayHash' => 'Nette\Utils\ArrayHash',
+	'Nette\ArrayList' => 'Nette\Utils\ArrayList',
+	'Nette\DateTime' => 'Nette\Utils\DateTime',
+	'Nette\Image' => 'Nette\Utils\Image',
+	'Nette\ObjectMixin' => 'Nette\Utils\ObjectMixin',
+	'Nette\Utils\NeonException' => 'Nette\Neon\Exception',
+	'Nette\Utils\NeonEntity' => 'Nette\Neon\Entity',
+	'Nette\Utils\Neon' => 'Nette\Neon\Neon',
+)), 'class_alias');
 
-require_once __DIR__ . '/aliases.php';
 require_once __DIR__ . '/shortcuts.php';
