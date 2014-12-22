db32c42b77bba21d79cc7ffa2cc0c623f44d6860
David Grudl <david@grudl.com>
2014-12-22 03:54:32 +0100

    loader: removed PHP version checking

--- Nette/loader.php
+++ Nette/loader.php
@@ -7,12 +7,6 @@
  */
 
 
-if (PHP_VERSION_ID < 50301) {
-	throw new Exception('Nette Framework requires PHP 5.3.1 or newer.');
-}
-
-
-// Run NetteLoader
 require_once __DIR__ . '/Loaders/NetteLoader.php';
 
 Nette\Loaders\NetteLoader::getInstance()->register();
