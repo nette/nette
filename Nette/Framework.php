d3a0af521cc5d09d4474ca51b9ba34ae910b83cf
David Grudl <david@grudl.com>
2014-12-22 04:36:29 +0100

    Framework: removed uselessness

--- Nette/Framework.php
+++ Nette/Framework.php
@@ -11,26 +11,14 @@ use Nette;
 
 
 /**
- * The Nette Framework (http://nette.org)
- *
- * @author     David Grudl
+ * Framework version.
  */
 class Framework
 {
 
-	/** Nette Framework version identification */
 	const NAME = 'Nette Framework',
 		VERSION = '2.3-dev',
 		VERSION_ID = 20300,
 		REVISION = '$WCREV$ released on $WCDATE$';
-
-
-	/**
-	 * Static class - cannot be instantiated.
-	 */
-	final public function __construct()
-	{
-		throw new StaticClassException;
-	}
 
 }
