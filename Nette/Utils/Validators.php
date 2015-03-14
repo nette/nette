nette/utils

Validators::isUrl: underscored subdomain support added

src/Utils/Validators.php
@@ -253,9 +253,13 @@ class Validators extends Nette\Object
 	public static function isUrl($value)
 	{
 		$alpha = "a-z\x80-\xFF";
+
+		$subDomain = "[-_0-9$alpha]";
 		$domain = "[0-9$alpha](?:[-0-9$alpha]{0,61}[0-9$alpha])?";
 		$topDomain = "[$alpha](?:[-0-9$alpha]{0,17}[$alpha])?";
-		return (bool) preg_match("(^https?://(?:(?:$domain\\.)*$topDomain|\\d{1,3}\.\\d{1,3}\.\\d{1,3}\.\\d{1,3}|\[[0-9a-f:]{3,39}\])(:\\d{1,5})?(/\\S*)?\\z)i", $value);
+
+		$basePath = "(?:(?:$subDomain+\\.)*?$domain\\.)?$topDomain";
+		return (bool) preg_match("(^https?://(?:$basePath|\\d{1,3}\.\\d{1,3}\.\\d{1,3}\.\\d{1,3}|\[[0-9a-f:]{3,39}\])(:\\d{1,5})?(/\\S*)?\\z)i", $value);
 	}
 
 

nette/nette

Nette/Utils/Validators.php
@@ -254,9 +254,11 @@ class Validators extends Nette\Object
 	public static function isUrl($value)
 	{
 		$alpha = "a-z\x80-\xFF";
+		$subDomain = "[-_0-9$alpha]";
 		$domain = "[0-9$alpha](?:[-0-9$alpha]{0,61}[0-9$alpha])?";
 		$topDomain = "[$alpha](?:[-0-9$alpha]{0,17}[$alpha])?";
-		return (bool) preg_match("(^https?://(?:(?:$domain\\.)*$topDomain|\\d{1,3}\.\\d{1,3}\.\\d{1,3}\.\\d{1,3}|\[[0-9a-f:]{3,39}\])(:\\d{1,5})?(/\\S*)?\\z)i", $value);
+		$domainName = "(?:(?:$subDomain+\\.)*?$domain\\.)?$topDomain";
+		return (bool) preg_match("(^https?://(?:$domainName|\\d{1,3}\.\\d{1,3}\.\\d{1,3}\.\\d{1,3}|\[[0-9a-f:]{3,39}\])(:\\d{1,5})?(/\\S*)?\\z)i", $value);
 	}
 
 