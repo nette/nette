70c39f5ce33c97bf5a08c0b1a9f24b2d5d8dc1e2
David Grudl <david@grudl.com>
2015-07-01 16:30:23 +0200

    Control: *global* snippet changed from NULL to \0 to be distinguished from ''

--- Nette/Application/UI/Control.php
+++ Nette/Application/UI/Control.php
@@ -127,7 +127,7 @@ abstract class Control extends PresenterComponent implements IRenderable
 	public function redrawControl($snippet = NULL, $redraw = TRUE)
 	{
 		if ($redraw) {
-			$this->invalidSnippets[$snippet] = TRUE;
+			$this->invalidSnippets[$snippet === NULL ? "\0" : $snippet] = TRUE;
 
 		} elseif ($snippet === NULL) {
 			$this->invalidSnippets = array();
@@ -182,7 +182,7 @@ abstract class Control extends PresenterComponent implements IRenderable
 			}
 
 		} else {
-			return isset($this->invalidSnippets[NULL]) || isset($this->invalidSnippets[$snippet]);
+			return isset($this->invalidSnippets["\0"]) || isset($this->invalidSnippets[$snippet]);
 		}
 	}
 
