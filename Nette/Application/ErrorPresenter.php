4d619875809c0bdc944f7ac0d0e8c09d63f0dcf1
David Grudl <david@grudl.com>
2015-07-12 16:24:22 +0200

    ErrorPresenter: exception is logged to 'exception.log'

--- Nette/Application/ErrorPresenter.php
+++ Nette/Application/ErrorPresenter.php
@@ -30,7 +30,7 @@ class ErrorPresenter extends Nette\Object implements Application\IPresenter
 			$code = $e->getCode();
 		} else {
 			$code = 500;
-			Debugger::log($e, Debugger::ERROR);
+			Debugger::log($e, Debugger::EXCEPTION);
 		}
 		ob_start();
 		require __DIR__ . '/templates/error.phtml';
