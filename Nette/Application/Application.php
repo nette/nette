af72a89976cd1aeeacccb942740f445752de6cae
David Grudl <david@grudl.com>
2020-08-25 01:05:03 +0200

    Application: Micro:* presenter are not achievable

--- Nette/Application/Application.php
+++ Nette/Application/Application.php
@@ -115,13 +115,15 @@ class Application extends Nette\Object
 
 		if (!$request instanceof Request) {
 			throw new BadRequestException('No route for HTTP request.');
-
-		} elseif (strcasecmp($request->getPresenterName(), $this->errorPresenter) === 0) {
+		}
+		$name = $request->getPresenterName();
+		if (strcasecmp($name, $this->errorPresenter) === 0
+			|| (Nette\Utils\Strings::startsWith($name, 'Nette:') && $name !== 'Nette:Micro')
+		) {
 			throw new BadRequestException('Invalid request. Presenter is not achievable.');
 		}
 
 		try {
-			$name = $request->getPresenterName();
 			$this->presenterFactory->getPresenterClass($name);
 			$request->setPresenterName($name);
 		} catch (InvalidPresenterException $e) {
